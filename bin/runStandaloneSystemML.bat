a23a0ee5009d3b4173c444bacddb0fb71e87b381
Christian Kadner <ckadner@us.ibm.com>
2015-09-14 17:40:52 -0700

    [SYSML-281] Simplify running in stand alone mode

---
+++ bin/runStandaloneSystemML.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+IF "%~1" == ""  GOTO Err
+IF "%~1" == "-help" GOTO Msg
+IF "%~1" == "-h" GOTO Msg
+
+setLocal EnableDelayedExpansion
+
+SET HADOOP_HOME=%CD%/lib/hadoop
+
+set CLASSPATH=./lib/*
+echo !CLASSPATH!
+
+for /f "tokens=1,* delims= " %%a in ("%*") do set ALLBUTFIRST=%%b
+
+java -Xmx4g -Xms4g -Xmn400m -cp %CLASSPATH% ^
+     com.ibm.bi.dml.api.DMLScript -f %1 -exec singlenode ^
+     -config=SystemML-config.xml %ALLBUTFIRST%
+GOTO End
+
+:Err
+ECHO "Wrong Usage. Please provide DML filename to be executed."
+GOTO Msg
+
+:Msg
+ECHO "Usage: runStandaloneSystemML.bat <dml-filename> [arguments] [-help]"
+ECHO "Script internally invokes 'java -Xmx4g -Xms4g -Xmn400m -jar jSystemML.jar -f <dml-filename> -exec singlenode -config=SystemML-config.xml [Optional-Arguments]'"
+
+:End
